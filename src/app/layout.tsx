d4c8043e2c525b8e044e14beb310cec645593955
Baleandor <overpoweredmedia@gmail.com>
2024-09-03 14:22:11 +0300

    Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import Header from "~/components/Header";
+import Footer from "~/components/Footer";
 
 export const metadata: Metadata = {
   title: "Victory Vault",
@@ -14,7 +16,11 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+      <body className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
